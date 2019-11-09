399be06108be90fa7f7c7e98981d6bf29119ecfc
Venil Noronha <veniln@vmware.com>
2019-11-08 16:10:45 -0800

    Fix cni path
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- release/create_release_archives.sh
+++ release/create_release_archives.sh
@@ -186,7 +186,7 @@ ls -l  "${COMMON_FILES_DIR}/install/kubernetes/helm/istio"
 
 TEMP_DIR=$(mktemp -d)
 pushd "${TEMP_DIR}"
-  git clone -b "${CB_BRANCH}" https://github.com/istio/cni.git
+  git clone -b "${CB_BRANCH}" https://github.com/vmware-allspark/cni.git
   cp -r cni/deployments/kubernetes/install/helm/istio-cni "${COMMON_FILES_DIR}/install/kubernetes/helm"
 
   git clone -b "${CB_BRANCH}" https://github.com/istio/operator.git

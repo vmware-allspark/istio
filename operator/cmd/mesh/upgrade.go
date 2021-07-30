d9078b8ef3bab1841d1c85e301988014b4249dfd
Sridharan K R <ksridharan@ksridharan-a01.vmware.com>
2021-07-30 14:39:46 +0530

    TSM-2348 - Ignore image check in telemtry pod during upgrade.

--- operator/cmd/mesh/upgrade.go
+++ operator/cmd/mesh/upgrade.go
@@ -516,7 +516,7 @@ func (client *Client) GetIstioVersions(namespace string) ([]ComponentVersion, er
 		switch component {
 		case "statsd-prom-bridge":
 			continue
-		case "mixer":
+		case "mixer", "telemetry":
 			continue
 		}
 
